6dd3ed522efb03fc41682294754f5f4181a83e70
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2023-07-04 07:34:03 +0200

    Update mtk_spm_idle.c

--- drivers/misc/mediatek/base/power/spm/mt6761/mtk_spm_idle.c
+++ drivers/misc/mediatek/base/power/spm/mt6761/mtk_spm_idle.c
@@ -14,10 +14,10 @@
 #endif
 //#include <trace/events/mtk_idle_event.h>
 
-#include <mtk_idle.h>
-#include <mtk_idle_internal.h>
-#include <mtk_spm_suspend_internal.h>
-#include <mtk_spm_resource_req.h>
+#include "mtk_idle.h"
+#include "mtk_idle_internal.h"
+#include "mtk_spm_suspend_internal.h"
+#include "mtk_spm_resource_req.h"
 
 #include "mtk_spm_internal.h"
 #include "pwr_ctrl.h"
