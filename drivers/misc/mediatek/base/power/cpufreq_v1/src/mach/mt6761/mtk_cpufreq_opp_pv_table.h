a72dd6113557ce062cdb09219380f3b26ab4cd1c
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2023-08-21 04:34:35 +0200

    Update mtk_cpufreq_opp_pv_table.h
    Changed some voltages in the hope it will boot up completely, without crashing at cpu initialization...

--- drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6761/mtk_cpufreq_opp_pv_table.h
+++ drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6761/mtk_cpufreq_opp_pv_table.h
@@ -10,9 +10,9 @@
 
 static unsigned int fyTbl[NR_FREQ * NR_MT_CPU_DVFS][ARRAY_COL_SIZE] = {
 	/* Freq, Vproc, post_div, clk_div */
-	{ 10000, 81, 1, 1 },	/* LL */
-	{ 5000, 76, 1, 1 },
-	{ 2500, 71, 1, 1 },
+	{ 10000, 150, 1, 1 },	/* LL */
+	{ 5000, 120, 1, 1 },
+	{ 2500, 100, 1, 1 },
 	{ 1767, 67, 1, 1 },
 	{ 1700, 63, 1, 1 },
 	{ 1633, 59, 1, 1 },
@@ -23,16 +23,16 @@ static unsigned int fyTbl[NR_FREQ * NR_MT_CPU_DVFS][ARRAY_COL_SIZE] = {
 	{ 1216, 37, 2, 1 },
 	{ 1125, 33, 2, 1 },
 	{ 1056, 30, 2, 1 },
-	{  100, 27, 2, 1 },
-	{   50, 24, 2, 1 },
-	{   25, 21, 2, 1 },
+	{  100, 25, 2, 1 },
+	{   50, 20, 2, 1 },
+	{   25, 15, 2, 1 },
 };
 
 static unsigned int sbTbl[NR_FREQ * NR_MT_CPU_DVFS][ARRAY_COL_SIZE] = {
 	/* Freq, Vproc, post_div, clk_div */
-        {10000, 81, 1, 1 },	/* LL */
-	{ 5000, 76, 1, 1 },
-	{ 2500, 71, 1, 1 },
+        { 10000, 150, 1, 1 },	/* LL */
+	{ 5000, 120, 1, 1 },
+	{ 2500, 100, 1, 1 },
 	{ 1889, 67, 1, 1 },
 	{ 1800, 63, 1, 1 },
 	{ 1711, 59, 1, 1 },
@@ -45,14 +45,14 @@ static unsigned int sbTbl[NR_FREQ * NR_MT_CPU_DVFS][ARRAY_COL_SIZE] = {
 	{ 1125, 33, 2, 1 },
 	{ 1033, 29, 2, 1 },
 	{ 100, 25, 2, 1 },
-	{ 25, 21, 2, 1 },
+	{ 25, 15, 2, 1 },
 };
 
 static unsigned int fy2Tbl[NR_FREQ * NR_MT_CPU_DVFS][ARRAY_COL_SIZE] = {
 	/* Freq, Vproc, post_div, clk_div */
-        { 10000, 81, 1, 1 },	/* LL */
-	{ 5000, 76, 1, 1 },
-	{ 2500, 71, 1, 1 },
+        { 10000, 150, 1, 1 },	/* LL */
+	{ 5000, 120, 1, 1 },
+	{ 2500, 100, 1, 1 },
 	{ 1767, 67, 1, 1 },
 	{ 1700, 63, 1, 1 },
 	{ 1633, 59, 1, 1 },
@@ -63,16 +63,16 @@ static unsigned int fy2Tbl[NR_FREQ * NR_MT_CPU_DVFS][ARRAY_COL_SIZE] = {
 	{ 1216, 45, 2, 1 },
 	{ 1125, 45, 2, 1 },
 	{ 1056, 45, 2, 1 },
-	{  100, 45, 2, 1 },
-	{  50, 45, 2, 1 },
-	{  25, 45, 2, 1 },
+	{  100, 25, 2, 1 },
+	{  50, 20, 2, 1 },
+	{  25, 15, 2, 1 },
 };
 
 static unsigned int LiteTbl[NR_FREQ * NR_MT_CPU_DVFS][ARRAY_COL_SIZE] = {
 	/* Freq, Vproc, post_div, clk_div */
-	{ 10000, 69, 1, 1 },	/* LL */
-	{ 5000, 67, 1, 1 },
-	{ 2500, 65, 1, 1 },
+	{ 10000, 150, 1, 1 },	/* LL */
+	{ 5000, 120, 1, 1 },
+	{ 2500, 100, 1, 1 },
 	{ 1700, 63, 1, 1 },
 	{ 1667, 61, 1, 1 },
 	{ 1633, 59, 1, 1 },
@@ -83,9 +83,9 @@ static unsigned int LiteTbl[NR_FREQ * NR_MT_CPU_DVFS][ARRAY_COL_SIZE] = {
 	{ 1216, 37, 2, 1 },
 	{ 1125, 33, 2, 1 },
 	{ 1056, 30, 2, 1 },
-	{  100, 27, 2, 1 },
-	{  50, 24, 2, 1 },
-	{  25, 21, 2, 1 },
+	{  100, 25, 2, 1 },
+	{  50, 20, 2, 1 },
+	{  25, 15, 2, 1 },
 };
 
 unsigned int *xrecordTbl[NUM_CPU_LEVEL] = {	/* v0.3 */
