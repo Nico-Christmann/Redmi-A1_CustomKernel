8d1b7ee34b5e03e321e4da8c28da009647229bc6
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2023-09-11 06:45:08 +0200

    Update mtk_gpufreq_core.h

--- drivers/gpu/mediatek/gpufreq/mt6761/mtk_gpufreq_core.h
+++ drivers/gpu/mediatek/gpufreq/mt6761/mtk_gpufreq_core.h
@@ -9,13 +9,13 @@
 /**************************************************
  * MT6761 : GPU DVFS OPP table Setting
  **************************************************/
-#define GPU_DVFS_FREQ0			(1100000)/* KHz */
+#define GPU_DVFS_FREQ0			(1000000)/* KHz */
 #define GPU_DVFS_FREQ1			(500000)/* KHz */
 #define GPU_DVFS_FREQ2			(150000)/* KHz */
 
-#define GPU_DVFS_VOLT0			(150000)	/* mV x 100 */
-#define GPU_DVFS_VOLT1			(75000)	/* mV x 100 */
-#define GPU_DVFS_VOLT2			(45000)	/* mV x 100 */
+#define GPU_DVFS_VOLT0			(95000)	/* mV x 100 */
+#define GPU_DVFS_VOLT1			(85000)	/* mV x 100 */
+#define GPU_DVFS_VOLT2			(75000)	/* mV x 100 */
 
 #define GPU_DVFS_VSRAM0			(90000)	/* mV x 100 */
 #define GPU_DVFS_VSRAM1			(80000)	/* mV x 100 */
