a1236e6fb2522af84def5c9ef7d3f2c2e2286f77
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2023-09-16 18:23:02 +0200

    Update mtk_cpufreq_opp_pv_table.h

--- drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6761/mtk_cpufreq_opp_pv_table.h
+++ drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6761/mtk_cpufreq_opp_pv_table.h
@@ -10,9 +10,9 @@
 
 static unsigned int fyTbl[NR_FREQ * NR_MT_CPU_DVFS][ARRAY_COL_SIZE] = {
 	/* Freq, Vproc, post_div, clk_div */
-	{ 2500, 300, 1, 1 },	/* LL */
-	{ 2250, 200, 1, 1 },
-	{ 2000, 100, 1, 1 },
+	{ 3500, 150, 1, 1 },	/* LL */
+	{ 3000, 125, 1, 1 },
+	{ 2500, 100, 1, 1 },
 	{ 1767, 67, 1, 1 },
 	{ 1700, 63, 1, 1 },
 	{ 1633, 59, 1, 1 },
@@ -30,9 +30,9 @@ static unsigned int fyTbl[NR_FREQ * NR_MT_CPU_DVFS][ARRAY_COL_SIZE] = {
 
 static unsigned int sbTbl[NR_FREQ * NR_MT_CPU_DVFS][ARRAY_COL_SIZE] = {
 	/* Freq, Vproc, post_div, clk_div */
-        { 2500, 300, 1, 1 },	/* LL */
-	{ 2250, 200, 1, 1 },
-	{ 2000, 100, 1, 1 },
+        { 3500, 150, 1, 1 },	/* LL */
+	{ 3000, 125, 1, 1 },
+	{ 2500, 100, 1, 1 },
 	{ 1889, 67, 1, 1 },
 	{ 1800, 63, 1, 1 },
 	{ 1711, 59, 1, 1 },
@@ -50,9 +50,9 @@ static unsigned int sbTbl[NR_FREQ * NR_MT_CPU_DVFS][ARRAY_COL_SIZE] = {
 
 static unsigned int fy2Tbl[NR_FREQ * NR_MT_CPU_DVFS][ARRAY_COL_SIZE] = {
 	/* Freq, Vproc, post_div, clk_div */
-        { 2500, 300, 1, 1 },	/* LL */
-	{ 2250, 200, 1, 1 },
-	{ 2000, 100, 1, 1 },
+        { 3500, 150, 1, 1 },	/* LL */
+	{ 3000, 125, 1, 1 },
+	{ 2500, 100, 1, 1 },
 	{ 1767, 67, 1, 1 },
 	{ 1700, 63, 1, 1 },
 	{ 1633, 59, 1, 1 },
@@ -70,9 +70,9 @@ static unsigned int fy2Tbl[NR_FREQ * NR_MT_CPU_DVFS][ARRAY_COL_SIZE] = {
 
 static unsigned int LiteTbl[NR_FREQ * NR_MT_CPU_DVFS][ARRAY_COL_SIZE] = {
 	/* Freq, Vproc, post_div, clk_div */
-	{ 2500, 300, 1, 1 },	/* LL */
-	{ 2250, 200, 1, 1 },
-	{ 2000, 100, 1, 1 },
+	{ 3500, 150, 1, 1 },	/* LL */
+	{ 3000, 125, 1, 1 },
+	{ 2500, 100, 1, 1 },
 	{ 1700, 63, 1, 1 },
 	{ 1667, 61, 1, 1 },
 	{ 1633, 59, 1, 1 },
