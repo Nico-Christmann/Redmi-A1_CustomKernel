b7ae951c0e97a1b77df2d2ee627ffcbbff0ad443
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2023-09-19 04:01:03 +0200

    Update mtk_cpufreq_opp_pv_table.h

--- drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6761/mtk_cpufreq_opp_pv_table.h
+++ drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6761/mtk_cpufreq_opp_pv_table.h
@@ -52,7 +52,7 @@ static unsigned int fy2Tbl[NR_FREQ * NR_MT_CPU_DVFS][ARRAY_COL_SIZE] = {
 	/* Freq, Vproc, post_div, clk_div */
         { 2050, 70, 1, 1 },	/* LL */
 	{ 2025, 70, 1, 1 },
-	{ 2015 70, 1, 1 },
+	{ 2015, 70, 1, 1 },
 	{ 1767, 67, 1, 1 },
 	{ 1700, 63, 1, 1 },
 	{ 1633, 59, 1, 1 },
